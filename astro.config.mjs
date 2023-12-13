8e3a34ef47155fd0bb5bd0e2b899968bfd45cbd3
sindhueswaran <54274176+sindhueswaran@users.noreply.github.com>
2023-12-13 12:21:29 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from '@astrojs/sitemap';
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sitemap()]
+  integrations: [tailwind(), react(), sitemap()],
+  output: "server",
+  adapter: netlify()
 });
